76c58276cdd65d870b9fa4c90e660468e780f647
UncleChair <voidgun1998@gmail.com>
2024-08-06 10:28:13 +0800

    Feature: add logout function

--- backend/internal/controller/auth/auth_v1_logout.go
+++ backend/internal/controller/auth/auth_v1_logout.go
@@ -3,12 +3,13 @@ package auth
 import (
 	"context"
 
-	"github.com/gogf/gf/v2/errors/gcode"
-	"github.com/gogf/gf/v2/errors/gerror"
+	"github.com/gogf/gf/v2/frame/g"
 
-	"backend/api/auth/v1"
+	v1 "backend/api/auth/v1"
 )
 
 func (c *ControllerV1) Logout(ctx context.Context, req *v1.LogoutReq) (res *v1.LogoutRes, err error) {
-	return nil, gerror.NewCode(gcode.CodeNotImplemented)
+	r := g.RequestFromCtx(ctx)
+	r.Session.RemoveAll()
+	return
 }
