07ed9987daea4bbbfd8b70e0f55717cca0e4b6d0
UncleChair <voidgun1998@gmail.com>
2024-08-07 11:37:11 +0800

    Feature: add word index function

--- backend/internal/controller/word/word_v1_index_word.go
+++ backend/internal/controller/word/word_v1_index_word.go
@@ -3,12 +3,15 @@ package word
 import (
 	"context"
 
-	"github.com/gogf/gf/v2/errors/gcode"
-	"github.com/gogf/gf/v2/errors/gerror"
-
-	"backend/api/word/v1"
+	v1 "backend/api/word/v1"
+	"backend/internal/dao"
 )
 
 func (c *ControllerV1) IndexWord(ctx context.Context, req *v1.IndexWordReq) (res *v1.IndexWordRes, err error) {
-	return nil, gerror.NewCode(gcode.CodeNotImplemented)
+	res = &v1.IndexWordRes{}
+	err = dao.Words.Ctx(ctx).Scan(res)
+	if err != nil {
+		return nil, err
+	}
+	return res, nil
 }
