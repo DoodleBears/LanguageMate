817d75c884385087da18435b0816a8bbac7fd6b9
UncleChair <voidgun1998@gmail.com>
2024-08-07 11:48:00 +0800

    Feature: add retrieve word function

--- backend/internal/controller/word/word_v1_retrieve_word.go
+++ backend/internal/controller/word/word_v1_retrieve_word.go
@@ -3,12 +3,18 @@ package word
 import (
 	"context"
 
-	"github.com/gogf/gf/v2/errors/gcode"
-	"github.com/gogf/gf/v2/errors/gerror"
+	v1 "backend/api/word/v1"
+	"backend/internal/dao"
 
-	"backend/api/word/v1"
+	"github.com/gogf/gf/v2/frame/g"
 )
 
 func (c *ControllerV1) RetrieveWord(ctx context.Context, req *v1.RetrieveWordReq) (res *v1.RetrieveWordRes, err error) {
-	return nil, gerror.NewCode(gcode.CodeNotImplemented)
+	res = &v1.RetrieveWordRes{}
+	id := g.RequestFromCtx(ctx).GetRouter("id").Int()
+	err = dao.Words.Ctx(ctx).Where("id", id).Scan(res)
+	if err != nil {
+		return nil, err
+	}
+	return res, nil
 }
