11b303576413531205b5f4582b2af75309c1c0c9
UncleChair <voidgun1998@gmail.com>
2024-08-07 12:21:31 +0800

    Feature: add pagination for word index

--- backend/api/word/v1/index.go
+++ backend/api/word/v1/index.go
@@ -4,7 +4,14 @@ import (
 	"github.com/gogf/gf/v2/frame/g"
 )
 
+type WordsList []*Word
 type IndexWordReq struct {
-	g.Meta `path:"/" tags:"Word" method:"get" sum:"Get a word"`
+	g.Meta  `path:"/" tags:"Word" method:"get" sum:"Get a word"`
+	Page    int `json:"page" des:"Page" in:"query"`
+	PerPage int `json:"per_page" des:"Page limit" in:"query"`
 }
-type IndexWordRes []*Word
+type IndexWordRes struct {
+	Words WordsList `json:"words" des:"Word list"`
+	Page  int       `json:"page" des:"Current page number"`
+	Total int       `json:"total" des:"Total page number"`
+}
--- backend/internal/controller/word/word_v1_index_word.go
+++ backend/internal/controller/word/word_v1_index_word.go
@@ -9,9 +9,20 @@ import (
 
 func (c *ControllerV1) IndexWord(ctx context.Context, req *v1.IndexWordReq) (res *v1.IndexWordRes, err error) {
 	res = &v1.IndexWordRes{}
-	err = dao.Words.Ctx(ctx).Scan(res)
+	var list v1.WordsList
+
+	if req.Page <= 0 || req.PerPage <= 0 {
+		err = dao.Words.Ctx(ctx).Scan(&list)
+	} else {
+		err = dao.Words.Ctx(ctx).OmitEmpty().
+			Order("id", "asc").
+			Limit((req.Page-1)*req.PerPage, req.PerPage).
+			ScanAndCount(&list, &res.Total, true)
+	}
 	if err != nil {
 		return nil, err
 	}
+	res.Page = req.Page
+	res.Words = list
 	return res, nil
 }
