b5b84bf72751f8b5b2d1892f448774f4eaac6005
UncleChair <voidgun1998@gmail.com>
2024-08-08 10:21:16 +0800

    Feature: add sentence index function

--- backend/api/sentence/v1/index.go
+++ backend/api/sentence/v1/index.go
@@ -12,10 +12,12 @@ type SentenceList []*Sentence
 
 // parameters should use uppercase
 type IndexSentenceReq struct {
-	g.Meta `path:"/" tags:"Sentence" method:"post" sum:"Get sentences"`
+	g.Meta  `path:"/" tags:"Sentence" method:"get" sum:"Get sentences"`
+	Page    int `json:"page" des:"Page" in:"query"`
+	PerPage int `json:"per_page" des:"Page limit" in:"query"`
 }
 type IndexSentenceRes struct {
-	Words SentenceList `json:"sentences" des:"Sentence list"`
-	Page  int          `json:"page" des:"Current page number"`
-	Total int          `json:"total" des:"Total page number"`
+	Sentences SentenceList `json:"sentences" des:"Sentence list"`
+	Page      int          `json:"page" des:"Current page number"`
+	Total     int          `json:"total" des:"Total page number"`
 }
--- backend/internal/controller/sentence/sentence_v1_index_sentence.go
+++ backend/internal/controller/sentence/sentence_v1_index_sentence.go
@@ -3,12 +3,38 @@ package sentence
 import (
 	"context"
 
-	"github.com/gogf/gf/v2/errors/gcode"
-	"github.com/gogf/gf/v2/errors/gerror"
+	v1 "backend/api/sentence/v1"
+	"backend/internal/dao"
 
-	"backend/api/sentence/v1"
+	"github.com/gogf/gf/v2/frame/g"
 )
 
 func (c *ControllerV1) IndexSentence(ctx context.Context, req *v1.IndexSentenceReq) (res *v1.IndexSentenceRes, err error) {
-	return nil, gerror.NewCode(gcode.CodeNotImplemented)
+	r := g.RequestFromCtx(ctx)
+	res = &v1.IndexSentenceRes{}
+	var list v1.SentenceList
+	total := 0
+	if req.Page <= 0 || req.PerPage <= 0 {
+		err = dao.Sentences.Ctx(ctx).Where("user_uid", r.Session.MustGet("uid").String()).Scan(&list)
+	} else {
+		err = dao.Sentences.Ctx(ctx).Where("user_uid", r.Session.MustGet("uid")).OmitEmpty().
+			Order("id", "asc").
+			Limit((req.Page-1)*req.PerPage, req.PerPage).
+			ScanAndCount(&list, &total, true)
+	}
+	if err != nil {
+		return nil, err
+	}
+	if req.Page > 0 && req.PerPage > 0 {
+		res.Page = req.Page
+		remainder := total % req.PerPage
+		brefPage := total / req.PerPage
+		if remainder == 0 {
+			res.Total = brefPage
+		} else {
+			res.Total = brefPage + 1
+		}
+	}
+	res.Sentences = list
+	return res, nil
 }
