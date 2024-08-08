18fbf86fef4a12e649688f9cb5ee81348da480d7
UncleChair <voidgun1998@gmail.com>
2024-08-08 10:14:28 +0800

    Feature: add index sentences api structure

--- backend/api/sentence/sentence.go
+++ backend/api/sentence/sentence.go
@@ -12,4 +12,5 @@ import (
 
 type ISentenceV1 interface {
 	CreateSentence(ctx context.Context, req *v1.CreateSentenceReq) (res *v1.CreateSentenceRes, err error)
+	RetrieveSentence(ctx context.Context, req *v1.RetrieveSentenceReq) (res *v1.RetrieveSentenceRes, err error)
 }
---
+++ backend/api/sentence/v1/index.go
@@ -0,0 +1,21 @@
+package v1
+
+import (
+	"github.com/gogf/gf/v2/frame/g"
+)
+
+type Sentence struct {
+	WordId  uint   `json:"word_id" v:"required" des:"Related word id" eg:"1"`
+	Content string `json:"content" des:"Word content text" eg:"We need to abandon the car."`
+}
+type SentenceList []*Sentence
+
+// parameters should use uppercase
+type RetrieveSentenceReq struct {
+	g.Meta `path:"/" tags:"Sentence" method:"post" sum:"Get sentences"`
+}
+type RetrieveSentenceRes struct {
+	Words SentenceList `json:"sentences" des:"Sentence list"`
+	Page  int          `json:"page" des:"Current page number"`
+	Total int          `json:"total" des:"Total page number"`
+}
