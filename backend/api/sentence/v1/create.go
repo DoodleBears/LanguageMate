8500ddc2b8e33fefddf93a0802e025b1137d9221
UncleChair <voidgun1998@gmail.com>
2024-08-08 10:03:53 +0800

    Pref: change detail value type

--- backend/api/sentence/v1/create.go
+++ backend/api/sentence/v1/create.go
@@ -7,7 +7,7 @@ import (
 // parameters should use uppercase
 type CreateSentenceReq struct {
 	g.Meta  `path:"/" tags:"Sentence" method:"post" sum:"Create a sentence"`
-	WordId  int    `json:"word_id" v:"required" des:"Related word id" eg:"1"`
+	WordId  uint   `json:"word_id" v:"required" des:"Related word id" eg:"1"`
 	Content string `json:"content" v:"required|length:1,200" des:"Sentence content text" eg:"This is a sentence."`
 }
 type CreateSentenceRes struct{}
